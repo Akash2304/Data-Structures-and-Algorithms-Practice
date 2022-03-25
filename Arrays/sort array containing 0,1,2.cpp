4b7fdc2d9980b109de9be55a1e9c205177f99b21
Akash Rajpoot <54078472+Akash2304@users.noreply.github.com>
2022-03-25 14:56:56 +0530

    Update sort array containing 0,1,2.cpp

--- Arrays/sort array containing 0,1,2.cpp
+++ Arrays/sort array containing 0,1,2.cpp
@@ -1,4 +1,4 @@
-/*sort-an-array-of-0s-1s-and-2s
+/*sort-an-array-of-0s-1s-and-2s******************************************************************************************************************************
 
 Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
 
@@ -27,7 +27,7 @@ into ascending order.
 Your Task:
 You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
 
-*/
+****************************************************************************************************************************************/
 
 class Solution
 {
