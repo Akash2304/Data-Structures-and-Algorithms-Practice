68bbde9428490153049cb9c754d2cbf22bdc071f
Akash Rajpoot <54078472+Akash2304@users.noreply.github.com>
2022-03-29 07:48:17 +0530

    Create find first set bit.cpp

---
+++ Bit Magic/find first set bit.cpp
@@ -0,0 +1,45 @@
+/********************************************************************************************************************************************************
+Given an integer an N. The task is to return the position of first set bit found from the right side in the binary representation of the number.
+Note: If there is no set bit in the integer N, then return 0 from the function.  
+
+Example 1:
+
+Input: N = 18
+Output: 2
+Explanation: Binary representation of 
+18 is 010010,the first set bit from the 
+right side is at position 2.
+Example 2:
+
+Input: N = 12 
+Output: 3 
+Explanation: Binary representation 
+of  12 is 1100, the first set bit 
+from the right side is at position 3.
+
+************************************************************************************************************************************************/
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        unsigned int count=1;
+        if(n==0)
+        return 0;
+        else
+        {
+        while(n)
+        {
+            if((n&1)==0)
+            {
+            count++;
+            n=n>>1;
+            }
+            else
+            break;
+        }
+        return count;
+        }
+    }
+};
