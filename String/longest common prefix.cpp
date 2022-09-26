044149d5a00ccfd67a644f44051f0df3cd794227
Akash Rajpoot <54078472+Akash2304@users.noreply.github.com>
2022-09-26 23:11:21 +0530

    Update longest common prefix.cpp

--- String/longest common prefix.cpp
+++ String/longest common prefix.cpp
@@ -48,3 +48,23 @@ public:
     
     }
 };
+
+/**************************************************************************************************************************************************************/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      string s;
+        for(int i=0;i<strs[0].size();i++)
+      {
+          char c=strs[0][i];
+            for(int j=0;j<strs.size();j++)
+            {
+                if(strs[j][i]!=c)
+                    return s;
+            }
+            s+=c;
+      }
+        return s;
+    }
+};
